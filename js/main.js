8dfd7b6f921cc01dc3bb937966d04f8504b5ac7c
FilipHarald <filip.harald@gmail.com>
2017-01-04 23:54:06 +0100

    Added default serach, error if one is not found and changed name of spotifyUri

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const App = React.createClass({
   getInitialState() {
     return {
-      spotifyUri: '',
-      giphyUrl: ''
+      spotifyUrl: 'https://embed.spotify.com/?uri=spotify:track:57bgtoPSgt236HzfBOd8kj',
+      giphyUrl: 'http://media2.giphy.com/media/3rgXBFhCIH3zlOua8o/giphy.gif'
     };
   },
   render() {
@@ -17,7 +17,7 @@ const App = React.createClass({
         <SearchBox  handleSubmit={this.handleSubmit}
                     handleChangeText={this.handleChangeText}/>
         <br/>
-        <ResultBox  spotifyUri={this.state.spotifyUri}
+        <ResultBox  spotifyUrl={this.state.spotifyUrl}
                     giphyUrl={this.state.giphyUrl}/>
       </div>
     );
@@ -35,7 +35,7 @@ const App = React.createClass({
     var spotifyResponse = $.getJSON('https://api.spotify.com/v1/search?q=' + text + '&type=track')
     var giphyResponse = $.getJSON('http://api.giphy.com/v1/gifs/search?q=' + text + '&api_key=dc6zaTOxFJmzC');
     spotifyResponse.then((data) => {
-      this.setState({spotifyUri: 'https://embed.spotify.com/?uri=' + data.tracks.items[0].uri});
+      this.setState({spotifyUrl: 'https://embed.spotify.com/?uri=' + data.tracks.items[0].uri});
     });
     giphyResponse.then((data) => {
       this.setState({giphyUrl: data.data[0].images.original.url});
@@ -48,7 +48,8 @@ const SearchBox = React.createClass({
     return (
       <div className="SearchBox">
         <form onSubmit={this.props.handleSubmit}>
-          <input type="text" onChange={this.props.handleChangeText}/>
+          <input  type="text"
+                  onChange={this.props.handleChangeText}/>
           <button type="submit"
                   className="submit-btn">Search!</button>
         </form>
@@ -59,15 +60,15 @@ const SearchBox = React.createClass({
 
 const ResultBox = React.createClass({
   render() {
-    if (this.props.spotifyUri && this.props.giphyUrl) {
+    if (this.props.spotifyUrl && this.props.giphyUrl) {
       return(
         <div className="ResultBox">
-          <iframe src={this.props.spotifyUri} width="300" height="380"></iframe>
+          <iframe src={this.props.spotifyUrl} width="300" height="380"></iframe>
           <img src={this.props.giphyUrl} alt="No gif? :(" height="380" width="380"/>
         </div>
       )
     }
-    return (<div className="ResultBox"></div>);
+    return (<div className="ResultBox">Could not find a song and gif on the search.</div>);
   }
 });
 
